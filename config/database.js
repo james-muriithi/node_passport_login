d7e77be0428d40b01026541230d35b63c8efa862
james-muriithi <muriithijames556@gmail.com>
2020-05-25 01:02:17 +0300

    added a database connection

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+module.exports = Sequelize({
+    database: process.env.DB,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    dialect: 'postgres'
+})
